29eacf3621b6ce88a580ab17c12d451c81b35628
Arthur-Savio-Dev <arthur.savio.pessoal@gmail.com>
2019-04-08 13:16:44 -0300

    changes

--- src/math_functions.py
+++ src/math_functions.py
@@ -1,25 +1,19 @@
 import math
+import sympy
 
 class MathFunctions:
-    def mdc(self, n1, n2):
-        if n1 % n2 == 0:
-            return n2
-        self.mdc(n1, n1 % n2)
-
-    def is_co_prime(self, n1, n2):
-        flag = self.mdc(n1, n2)
-        if flag:
+    def is_prime(self, num):
+        if sympy.isprime(num):
             return True
         return False
-
-    def is_prime(self, num):
-        for i in range(2, int(math.sqrt(num) + 1)):
-            if num % i == 0:
-                return False
-        return True
 
     def inverse_multiplicative(self, e, totiente):
         for i in range(0, totiente-1):
             if (i*e) % totiente == 1:
                 return i
 
+    def generate_random_prime_numbers(self):
+            p = sympy.randprime(100, 1000)
+            q = sympy.randprime(100, 1000)
+            e = sympy.randprime(100, 1000)
+            return p, q, e
--- src/rsa.py
+++ src/rsa.py
@@ -1,12 +1,37 @@
+from math_functions import MathFunctions
+
 class Rsa:
-    def generate_key(self):
-        pass
+    def __init__(self):
+        self.mf = MathFunctions()
+        self.public_key = tuple()
+        self.private_key = tuple()
+        self.encrypted_message = list()
+        self.decrypted_message = list()
+        self.setup()
 
-    def character_to_number(self):
-        pass
+    def setup(self):
+        p, q, e = list(self.mf.generate_random_prime_numbers())
+        n = p * q
+        d = self.mf.inverse_multiplicative(e, (p-1) * (q-1))
 
-    def encrypt(self):
-        pass
+        self.private_key = p, q, d
+        self.public_key = n, e
 
-    def decrypt(self):
-        pass
+    def encrypt(self, message):
+        list_num = [ord(char) for char in message]
+        for i in list_num:
+            value = pow(i, self.public_key[1])
+            mod = value % self.public_key[0]
+            self.encrypted_message.append(mod)
+
+    def decrypt(self, message_list):
+        d = self.private_key[2]
+        n = self.public_key[0]
+        for i in message_list:
+            result = pow(i, d) % n
+            self.decrypted_message.append(chr(result))
+
+rsa = Rsa()
+rsa.encrypt("minha casa e grande")
+rsa.decrypt(rsa.encrypted_message)
+print(rsa.decrypted_message)
